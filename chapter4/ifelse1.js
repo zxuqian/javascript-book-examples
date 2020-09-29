a502620a67c5d161bf035df8befb70279a5147e6
Xuqian Zhang <zxuqian@gmail.com>
2020-09-29 18:02:38 +0800

    添加第 4 章示例代码
     ifelse

---
+++ chapter4/ifelse1.js
@@ -0,0 +1,6 @@
+let a = 5;
+if (a > 5) {
+  console.log("a 大于 5");
+} else {
+  console.log("a 小于等于 5");
+}
---
+++ chapter4/ifelse2.js
@@ -0,0 +1,6 @@
+let a = "";
+if (a) {
+  console.log("a 有值");
+} else {
+  console.log("a 无值");
+}
