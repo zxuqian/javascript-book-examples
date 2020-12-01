46b92cb6625ee91fcc26d10533e13f24f0e8fac6
Xuqian Zhang <zxuqian@gmail.com>
2020-12-01 18:02:29 +0800

    添加第 10 章示例
    Symbol

---
+++ chapter10/symbol_match1.js
@@ -0,0 +1,11 @@
+const mp3FilePattern = {
+  [Symbol.match](str) {
+    return str.endsWith(".mp3");
+  },
+};
+
+let res = "song.mp3".match(mp3FilePattern);
+let res2 = "text.txt".match(mp3FilePattern);
+console.log(res, res2); // true false
+
+console.log("text.txt".search(mp3FilePattern));
---
+++ chapter10/typed_array2.js
@@ -0,0 +1,10 @@
+let buffer = new ArrayBuffer(4);
+let dataView = new DataView(buffer);
+
+console.log(dataView.setInt32(0, 0x1f320070));
+console.log(dataView.getInt16(0)); // 7986
+console.log(dataView.getInt16(2)); // 112
+console.log(dataView.getInt16(0, true)); // 12831
+console.log(dataView.getInt16(2, true)); // 28672
+
+console.log(dataView.getInt16(1)); // 12800
