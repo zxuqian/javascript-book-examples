4ea6049fcf9fe27767c21fdb259e522f530f3b26
Xuqian Zhang <zxuqian@gmail.com>
2020-12-03 17:58:40 +0800

    添加第 10 章示例
    proxy

--- chapter10/proxy5.js
+++ chapter10/proxy5.js
@@ -1,38 +1,9 @@
-let obj = {
-  a: 1,
-  _b: 2,
-  get B() {
-    return this._b;
-  },
-  set B(value) {
-    this._b = value;
-  },
-  sum(c) {
-    return this.a + this._b + c;
-  },
-};
-obj = new Proxy(obj, {
-  get(target, propName, receiver) {
-    console.log(`obj: 访问属性 ${propName}`);
-    return Reflect.get(target, propName, receiver);
-  },
-  set(target, propName, value, receiver) {
-    console.log(
-      `obj: 修改属性 ${propName}，旧值为 ${target[propName]}，新值为 ${value}`
-    );
-    return Reflect.set(target, propName, value, receiver);
-  },
+let findTwo = (x) => x === 2;
+findTwo = new Proxy(findTwo, {
   apply(target, thisArg, args) {
-    console.log(
-      `obj: 调用了方法 ${target}，this 为 ${thisArg}，参数列表为 ${args}`
-    );
+    console.log(`调用了函数 ${target}，this 为 ${thisArg}，参数为 ${args}`);
     return Reflect.apply(target, thisArg, args);
   },
 });
 
-obj.a;
-obj.a = 5;
-obj.b;
-obj.b = 8;
-console.log(obj.sum(10));
-Reflect.apply(obj.sum, obj, [10]);
+[1, 2, 3].find(findTwo);
