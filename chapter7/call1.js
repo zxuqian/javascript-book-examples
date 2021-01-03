fd5dfcea98df01b526533aa94d860e6394c4c0be
Xuqian Zhang <zxuqian@gmail.com>
2021-01-03 12:27:28 +0800

    更新示例

--- chapter7/call1.js
+++ chapter7/call1.js
@@ -1,9 +1,9 @@
-function add(prop1, prop2) {
+function sum(prop1, prop2) {
   return this[prop1] + this[prop2];
 }
 
-// console.log(add("a", "b")); // NaN
+// console.log(sum("a", "b")); // NaN
 
 const obj = { a: 1, b: 2 };
-const result = add.call(obj, "a", "b");
+const result = sum.call(obj, "a", "b");
 console.log(result); // 3
