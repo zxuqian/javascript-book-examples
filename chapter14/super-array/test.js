bdbc10cafe6f5168a3cc735d2557a72bdd26e934
Xuqian Zhang <zxuqian@gmail.com>
2021-03-17 13:05:57 +0800

    Update test.js

--- chapter14/super-array/test.js
+++ chapter14/super-array/test.js
@@ -14,16 +14,17 @@ import {
 const arr1 = [1, 3, 5, 7, 9];
 const arr2 = [3, 6, 3, 5, 9, 8, 2, 0, 1];
 
-const log = console.log;
+const log = (v) => console.log(JSON.stringify(v));
+
+log(unique(arr2));
 log(intersection(arr1, arr2));
 log(union(arr1, arr2));
 log(difference(arr1, arr2));
 log(difference_symm(arr1, arr2));
-log(sum(arr2));
+log(sum(arr1));
 log(mean(arr1));
 log(range(5, 10));
 log(split(arr2, 4));
-log(unique(arr2));
 
 const arr3 = [1, 3, 5, 5, 5, 3, 2, 2, 1, 1, 0, 5];
 log(count_values(arr3));
